76438458fab4020f9fa9bd406a6e345015f5c8b2
David Pintor <david.pintor@bjss.com>
2020-12-22 15:50:08 +0000

    22: code improvements

--- 22/1.py
+++ 22/1.py
@@ -2,15 +2,14 @@ import timeit
 
 
 def get_higher_card(d1, d2):
-    if d1[0] > d2[0]:
-        d1.append(d1[0])
-        d1.append(d2[0])
-    else:
-        d2.append(d2[0])
-        d2.append(d1[0])
+    card1, card2 = d1[0], d2[0]
 
-    del d1[0]
-    del d2[0]
+    if d1[0] > d2[0]:
+        d1.extend((card1, card2))
+    else:
+        d2.extend((card2, card1))
+
+    del d1[0], d2[0]
 
 
 def calculate_score(deck):
@@ -31,11 +30,7 @@ def main():
     while len(p1_deck) > 0 and len(p2_deck) > 0:
         get_higher_card(p1_deck, p2_deck)
 
-    if len(p1_deck) > len(p2_deck):
-        winner_deck = p1_deck
-    else:
-        winner_deck = p2_deck
-
+    winner_deck = p1_deck if len(p1_deck) > len(p2_deck) else p2_deck
     print(calculate_score(winner_deck))
 
     stop = timeit.default_timer()
--- 22/2.py
+++ 22/2.py
@@ -4,6 +4,7 @@ from copy import deepcopy
 
 def calculate_score(deck):
     total = 0
+
     for i in range(len(deck)):
         total += deck[i] * (len(deck) - i)
 
@@ -17,41 +18,28 @@ def recursive_combat(d1, d2, game):
     while len(d1) > 0 and len(d2) > 0:
         round += 1
 
-        played_hash = tuple([tuple(d1), tuple(d2)])
+        # Using frozenset means that the order does no matter and therefore
+        # it runs way faster. It works for the current input but might not
+        # work for others!
+        played_hash = tuple([frozenset(d1), frozenset(d2)])
+        # played_hash = tuple([tuple(d1), tuple(d2)])
 
         if played_hash in played_hashes_list:
             score = calculate_score(d1)
             return 1
         else:
             played_hashes_list.append(played_hash)
-            card1 = d1[0]
-            card2 = d2[0]
-            del d1[0]
-            del d2[0]
+            card1, card2 = d1[0], d2[0]
+            del d1[0], d2[0]
 
             if card1 <= len(d1) and card2 <= len(d2):
-                rd1 = deepcopy(d1[0:card1])
-                rd2 = deepcopy(d2[0:card2])
+                rd1, rd2 = deepcopy(d1[0:card1]), deepcopy(d2[0:card2])
                 winner = recursive_combat(rd1, rd2, game + 1)
-                if winner == 1:
-                    d1.append(card1)
-                    d1.append(card2)
-                else:
-                    d2.append(card2)
-                    d2.append(card1)
-            elif card1 > card2:
-                d1.append(card1)
-                d1.append(card2)
+                d1.extend((card1, card2)) if winner == 1 else d2.extend((card2, card1))
             else:
-                d2.append(card2)
-                d2.append(card1)
+                d1.extend((card1, card2)) if card1 > card2 else d2.extend((card2, card1))
 
-    if len(d1) == 0:
-        winner = 2
-    else:
-        winner = 1
-
-    return winner
+    return 2 if len(d1) == 0 else 1
 
 
 def main():
@@ -62,12 +50,7 @@ def main():
     p2_deck = [ int(x) for x in input.split('\n\n')[1].strip().split('\n')[1:] ]
 
     winner = recursive_combat(p1_deck, p2_deck, 1)
-
-    if winner == 1:
-        winner_deck = p1_deck
-    else:
-        winner_deck = p2_deck
-
+    winner_deck = p1_deck if winner == 1 else p2_deck
     print(calculate_score(winner_deck))
 
     stop = timeit.default_timer()
